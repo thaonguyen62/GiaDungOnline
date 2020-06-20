53ccf8ae9e5a96cb9d1f2bba5d34d97a36f67def
nguyenthiminh2603 <65128691+nguyenthiminh2603@users.noreply.github.com>
2020-06-20 23:45:10 +0700

    Miiiiinh commit

--- src/main/java/ltwwwjava/btl/dogiadungtructuyen/dto/OrderDTO.java
+++ src/main/java/ltwwwjava/btl/dogiadungtructuyen/dto/OrderDTO.java
@@ -1,19 +1,11 @@
 package ltwwwjava.btl.dogiadungtructuyen.dto;
 
 import lombok.Data;
-import ltwwwjava.btl.dogiadungtructuyen.model.Customer;
 
-import java.io.Serializable;
-import java.util.Date;
+import java.util.List;
 
 @Data
-public class OrderDTO implements Serializable {
-
-    private String id;
-    private int status;
-    private Date billDate;
-    private double tax;
-    private String customer;
-    private String products;
-    private int quantity;
+public class OrderDTO {
+    List<OrderDetailDTO> list;
+    double price;
 }
---
+++ src/main/java/ltwwwjava/btl/dogiadungtructuyen/dto/OrderDetailDTO.java
@@ -0,0 +1,20 @@
+package ltwwwjava.btl.dogiadungtructuyen.dto;
+
+import lombok.Data;
+import ltwwwjava.btl.dogiadungtructuyen.model.Customer;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class OrderDetailDTO implements Serializable {
+
+    private String id;
+    private int status;
+    private Date billDate;
+    private double tax;
+    private String customer;
+    private String products;
+    private int quantity;
+    private String orderId;
+}
