1c9f5e5c3501b8cb98a6a9ca92446d0ade77215b
Tu oanh <vothituoanh2911@gmail.com>
2020-07-01 23:27:03 +0700

    update editUser.html,listUser.html,Usercntroller

--- src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/UserController.java
+++ src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/UserController.java
@@ -13,8 +13,10 @@ import ltwwwjava.btl.dogiadungtructuyen.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -51,7 +53,7 @@ public class UserController {
     public String getlistEmployee(Model model) {
         List<Category> listCat = categoryRepository.findAll();
         model.addAttribute("categories",listCat);
-        List<User> userList = userService.findByAccountType(0);
+        List<User> userList = userService.findByAccountType(1);
         model.addAttribute("userList",userList);
         return "listUser";
     }
@@ -60,23 +62,71 @@ public class UserController {
     public String getlistCustomer(Model model) {
         List<Category> listCat = categoryRepository.findAll();
         model.addAttribute("categories",listCat);
-        List<User> userList = userService.findByAccountType(1);
+        List<User> userList = userService.findByAccountType(0);
         model.addAttribute("userList",userList);
         return "listUser";
     }
     @GetMapping("/editUser/{id}")
     public String geteditUser(@PathVariable(value = "id") String id, Model model) throws ResourceNotFoundException {
-       User personForm = userService.findById(id);
-
+       User user = userService.findById(id);
+       UserDTO personForm = new UserDTO();
+        personForm.setId(user.getId());
+        personForm.setName(user.getName());
+        personForm.setUsername(user.getUsername());
+        personForm.setAddress(user.getAddress());
+        personForm.setBirth(user.getBirth());
+        personForm.setPhone(user.getPhone());
+        personForm.setMail(user.getMail());
+        personForm.setPassword(user.getPassword());
+        personForm.setRetypePassword(user.getPassword());
+        personForm.setAccountType(user.getAccountType());
         model.addAttribute("personForm", personForm);
         return "editUser";
     }
-    @GetMapping("/deleteUser")
-    public String getdeleteUser(Model model) {
-        List<Category> listCat = categoryRepository.findAll();
-        UserDTO personForm = new UserDTO();
-        model.addAttribute("personForm", personForm);
-        return "listUser";
+
+    @PostMapping("/editUser/{id}")
+    public String editUserPost(Model model, @ModelAttribute("personForm") UserDTO personForm) throws ResourceNotFoundException {
+
+        if (!checkInvalid2(personForm).isInvalid()) {
+            model.addAttribute("errorMessage", checkInvalid(personForm).getMessage());
+        } else {
+            User user = userService.findById(personForm.getId());
+            user.setAccountType(personForm.getAccountType());
+            user.setAddress(personForm.getAddress());
+            user.setMail(personForm.getMail());
+            user.setPhone(personForm.getPhone());
+            user.setPassword(personForm.getPassword());
+            user.setName(personForm.getName());
+            user.setUsername(personForm.getUsername());
+            user.setBirth(personForm.getBirth());
+            userService.createAndUpdate(user);
+        List<User> userList = userService.findByAccountType(personForm.getAccountType());
+        model.addAttribute("userList",userList);
+        if(personForm.getAccountType()==1){
+            return "redirect:/employeeManager";
+        }else
+
+            return "redirect:/customerManager";
+        }
+        return "editUser";
+  }
+
+
+    @GetMapping("/deleteUser/{id}")
+    public String getdeleteUser(@PathVariable("id") String id, Model model) throws  ResourceNotFoundException{
+            User user = userService.findById(id);
+           if( user!=null){
+               userService.deleteCustomerById(id);
+           }
+        List<User> userList = userService.findByAccountType(user.getAccountType());
+        model.addAttribute("userList",userList);
+        if(user.getAccountType()==1){
+            return "redirect:/employeeManager";
+        }else
+
+            return "redirect:/customerManager";
+
+
     }
     @RequestMapping(value = {"/register"}, method = RequestMethod.GET)
     public String showAddPersonPage(Model model) {
@@ -101,6 +151,7 @@ public class UserController {
             user.setPassword(personForm.getPassword());
             user.setName(personForm.getName());
             user.setUsername(personForm.getUsername());
+            user.setBirth(personForm.getBirth());
             userService.createAndUpdate(user);
             return "redirect:/login";
         }
@@ -114,6 +165,23 @@ public class UserController {
             validateDTO.setMessage(Constants.USERNAME_EXIST);
             validateDTO.setInvalid(false);
         }
+        if (!personForm.getPassword().equals(personForm.getRetypePassword())) {
+            validateDTO.setMessage(Constants.INVALID_PASSWORD);
+            validateDTO.setInvalid(false);
+        }
+        return validateDTO;
+    }
+    private ValidateDTO checkInvalid2(UserDTO personForm) throws ResourceNotFoundException {
+        ValidateDTO validateDTO = new ValidateDTO();
+        validateDTO.setInvalid(true);
+        if(!personForm.getUsername().equalsIgnoreCase(userService.findById(personForm.getId()).getUsername())){
+
+            if (userService.checkUserNameIsExist(personForm.getUsername())) {
+                validateDTO.setMessage(Constants.USERNAME_EXIST);
+                validateDTO.setInvalid(false);
+            }
+        }
+
         if (!personForm.getPassword().equals(personForm.getRetypePassword())) {
             validateDTO.setMessage(Constants.INVALID_PASSWORD);
             validateDTO.setInvalid(false);
