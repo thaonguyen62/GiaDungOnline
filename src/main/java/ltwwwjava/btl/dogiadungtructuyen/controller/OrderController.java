4b8e40d0673e8197542551ace998bc64046656b6
nguyen <nguyenvlt0602@gmail.com>
2020-07-01 14:38:34 +0700

    update

--- src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/OrderController.java
+++ src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/OrderController.java
@@ -3,13 +3,17 @@ package ltwwwjava.btl.dogiadungtructuyen.controller;
 import ltwwwjava.btl.dogiadungtructuyen.exception.ResourceNotFoundException;
 import ltwwwjava.btl.dogiadungtructuyen.model.Category;
 import ltwwwjava.btl.dogiadungtructuyen.model.OrderDetail;
+import ltwwwjava.btl.dogiadungtructuyen.model.Product;
 import ltwwwjava.btl.dogiadungtructuyen.repository.CategoryRepository;
 import ltwwwjava.btl.dogiadungtructuyen.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -18,12 +22,26 @@ public class OrderController {
     private OrderService orderService;
     @Autowired
     private CategoryRepository categoryRepository;
+
     @GetMapping("/cart")
     public String getCart(Model model) throws ResourceNotFoundException {
-        List<OrderDetail> list=orderService.getAll();
-        model.addAttribute("listOrder",list);
+        List<OrderDetail> list = orderService.getAll();
+        model.addAttribute("listOrder", list);
         List<Category> listCat = categoryRepository.findAll();
         model.addAttribute("categories", listCat);
         return "cartt";
+    }
+
+    @PostMapping("/add-cart")
+    public String addToCart(Model model, @ModelAttribute("product") Product product) throws ResourceNotFoundException {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setBillDate(new Date());
+        orderDetail.setCustomer("1");
+        orderDetail.setProducts(product.getId());
+        orderDetail.setQuantity(15);
+        orderService.createAndUpdate(orderDetail);
+//        productService.createAndUpdate(product);
+        return "redirect:/products";
+
     }
 }
