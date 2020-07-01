fd2029965260e8df8cdb4518735c57223ef1fa92
Tu oanh <vothituoanh2911@gmail.com>
2020-07-01 10:39:38 +0700

    update about.html

--- src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/UserController.java
+++ src/main/java/ltwwwjava/btl/dogiadungtructuyen/controller/UserController.java
@@ -3,19 +3,33 @@ package ltwwwjava.btl.dogiadungtructuyen.controller;
 import ltwwwjava.btl.dogiadungtructuyen.dto.UserDTO;
 import ltwwwjava.btl.dogiadungtructuyen.dto.ValidateDTO;
 import ltwwwjava.btl.dogiadungtructuyen.exception.ResourceNotFoundException;
+import ltwwwjava.btl.dogiadungtructuyen.model.Category;
 import ltwwwjava.btl.dogiadungtructuyen.model.User;
+import ltwwwjava.btl.dogiadungtructuyen.repository.CategoryRepository;
+import ltwwwjava.btl.dogiadungtructuyen.repository.ProductRepository;
 import ltwwwjava.btl.dogiadungtructuyen.service.UserService;
+import ltwwwjava.btl.dogiadungtructuyen.service.impl.ProductImpl;
 import ltwwwjava.btl.dogiadungtructuyen.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    private ProductImpl productService;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+
+    //localhost:8087/houseware-service/products
 
     @GetMapping("/login")
     public String signIn(Model model) {
@@ -32,7 +46,38 @@ public class UserController {
         model.addAttribute("errorMessageLogin", Constants.USERNAME_PASSWORD_IS_INCORRECT);
         return "login";
     }
+    //nhanvien 0
+    @GetMapping("/employeeManager")
+    public String getlistEmployee(Model model) {
+        List<Category> listCat = categoryRepository.findAll();
+        model.addAttribute("categories",listCat);
+        List<User> userList = userService.findByAccountType(0);
+        model.addAttribute("userList",userList);
+        return "listUser";
+    }
+    //khach hang 1
+    @GetMapping("/customerManager")
+    public String getlistCustomer(Model model) {
+        List<Category> listCat = categoryRepository.findAll();
+        model.addAttribute("categories",listCat);
+        List<User> userList = userService.findByAccountType(1);
+        model.addAttribute("userList",userList);
+        return "listUser";
+    }
+    @GetMapping("/editUser/{id}")
+    public String geteditUser(@PathVariable(value = "id") String id, Model model) throws ResourceNotFoundException {
+       User personForm = userService.findById(id);
 
+        model.addAttribute("personForm", personForm);
+        return "editUser";
+    }
+    @GetMapping("/deleteUser")
+    public String getdeleteUser(Model model) {
+        List<Category> listCat = categoryRepository.findAll();
+        UserDTO personForm = new UserDTO();
+        model.addAttribute("personForm", personForm);
+        return "listUser";
+    }
     @RequestMapping(value = {"/register"}, method = RequestMethod.GET)
     public String showAddPersonPage(Model model) {
         UserDTO personForm = new UserDTO();
@@ -40,6 +85,7 @@ public class UserController {
         
         return "register";
     }
+
 
     @PostMapping("/register")
     public String register(Model model, @ModelAttribute("personForm") UserDTO personForm) throws ResourceNotFoundException {
@@ -78,3 +124,4 @@ public class UserController {
 
 
 }
+
